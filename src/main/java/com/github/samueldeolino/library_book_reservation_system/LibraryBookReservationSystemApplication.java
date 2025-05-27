24fb922409b2d026c6f91c237276ac5ba4156f77
Samuel Deolino <samueldeolino@alu.ufc.br>
2025-05-27 12:55:16 -0300

    chore: create project with spring boot initializr

---
+++ src/main/java/com/github/samueldeolino/library_book_reservation_system/LibraryBookReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.samueldeolino.library_book_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBookReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBookReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/samueldeolino/library_book_reservation_system/LibraryBookReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.samueldeolino.library_book_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBookReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
