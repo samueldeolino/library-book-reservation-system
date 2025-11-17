5cdd3d6814cd9cf1e988e016e95583d46d56cc2c
Samuel Deolino <samueldeolino@alu.ufc.br>
2025-11-17 15:27:18 -0300

    feat(db): populating the bd

---
+++ src/main/resources/db/migration/V4__insert_initial_users_and_books.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (name, phone_number)
+VALUES ('Ana Silva', '85911112222'),
+       ('Bruno Costa', '85933334444'),
+       ('Carlos Dias', '85955556666');
+
+INSERT INTO books (title, author, isbn, copies_available)
+VALUES
+('O Guia do Mochileiro das Galáxias', 'Douglas Adams', '9788575422468', 5),
+('1984', 'George Orwell', '9788535914849', 3),
+('Dom Casmurro', 'Machado de Assis', '9788525055318', 7),
+('A Cor Púrpura', 'Alice Walker', '9788503011988', 2),
+('O Senhor dos Anéis: A Sociedade do Anel', 'J.R.R. Tolkien', '9788595084759', 0),
+('A Hora da Estrela', 'Clarice Lispector', '9788532505672', 4),
+('Capitães da Areia', 'Jorge Amado', '9788535912494', 6),
+('O Alquimista', 'Paulo Coelho', '9788575427586', 10),
+('Cem Anos de Solidão', 'Gabriel García Márquez', '9788501012139', 3),
+('Duna', 'Frank Herbert', '9788576572522', 8),
+('Orgulho e Preconceito', 'Jane Austen', '9788525411728', 5),
+('Sapiens: Uma Breve História da Humanidade', 'Yuval Noah Harari', '9788543102176', 9);
